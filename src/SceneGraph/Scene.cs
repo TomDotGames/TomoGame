9a5a8879e130a0d48f38f05232770aa2c80ca5fd
TomDotGames <tdavies@gmail.com>
2025-10-05 22:47:07 -0700

    scenegraph initial commit

---
+++ src/SceneGraph/Scene.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Diagnostics;
+
+namespace TDG.SceneGraph
+{
+	public class Scene
+	{
+		public enum ESceneScaleMode
+		{
+			Fixed_Height,
+			Fixed_Width
+		}
+
+		public float ScaleFactor => m_flScaleFactor;
+		private float m_flScaleFactor;
+
+		public SceneNode RootNode => m_rootNode;
+		private SceneNode m_rootNode;
+
+		public GraphicsDevice Graphics => m_graphics.GraphicsDevice;
+		protected GraphicsDeviceManager m_graphics;
+
+		public Scene( GraphicsDeviceManager graphics, ESceneScaleMode eScaleMode, int nSize )
+		{
+			Debug.Assert( nSize > 0 );
+
+			// eventually we will want to be able to change the window size
+			float flWindowSize = eScaleMode == ESceneScaleMode.Fixed_Height ? graphics.PreferredBackBufferHeight : graphics.PreferredBackBufferWidth;
+			Debug.Assert( flWindowSize > 0 );
+
+			m_flScaleFactor = flWindowSize / nSize;
+			m_rootNode = new SceneNode( null, this );
+			m_graphics = graphics;
+		}
+
+		public virtual void Initialize()
+		{
+			RootNode.Initialize();
+		}
+
+		public virtual void Draw( GameTime gameTime, SpriteBatch spriteBatch )
+		{
+			Debug.Assert( spriteBatch != null );
+
+			float flDeltaTime = gameTime.ElapsedGameTime.Ticks / ( float )TimeSpan.TicksPerSecond;
+			RootNode.Draw( flDeltaTime, spriteBatch );
+		}
+
+		public virtual void Update( GameTime gameTime )
+		{
+			float flDeltaTime = gameTime.ElapsedGameTime.Ticks / ( float )TimeSpan.TicksPerSecond;
+			RootNode.Update( flDeltaTime );
+		}
+
+		public Vector2 WindowToSceneCoords( Vector2 vWindowCoords )
+		{
+			return vWindowCoords / m_flScaleFactor;
+		}
+	}
+}
---
+++ src/SceneGraph/SceneNode.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TDG.SceneGraph
+{
+    public class SceneNode
+    {
+        protected Scene OwnerScene => m_scene;
+        private Scene m_scene;
+
+        public Vector2 PositionWorld
+        {
+            get => m_vPositionWorld;
+            set
+            {
+                if (m_vPositionWorld != value)
+                {
+                    m_vPositionWorld = value;
+                    Vector2 vParentPosition = m_parent == null ? Vector2.Zero : m_parent.PositionWorld;
+                    m_vPositionLocal = vParentPosition - m_vPositionWorld;
+                    OnTransformChanged();
+                }
+            }
+        }
+        private Vector2 m_vPositionWorld;
+
+        public Vector2 PositionLocal
+        {
+            get => m_vPositionLocal;
+            set
+            {
+                if (m_vPositionLocal != value)
+                {
+                    m_vPositionLocal = value;
+                    Vector2 vParentPosition = m_parent == null ? Vector2.Zero : m_parent.PositionWorld;
+                    m_vPositionWorld = vParentPosition + m_vPositionLocal;
+                    OnTransformChanged();
+                }
+            }
+        }
+        private Vector2 m_vPositionLocal;
+
+        public SceneNode Parent => m_parent;
+        private SceneNode m_parent;
+        protected List<SceneNode> m_children = new List<SceneNode>();
+
+        public SceneNode(SceneNode parent, Scene scene)
+        {
+            parent?.AddChild(this);
+            m_scene = scene;
+            m_parent = parent;
+
+            Debug.Assert(m_scene != null);
+        }
+
+        public SceneNode(SceneNode parent)
+        {
+            parent?.AddChild(this);
+            m_scene = parent.m_scene;
+            m_parent = parent;
+
+            Debug.Assert(m_scene != null);
+        }
+
+        public virtual void Initialize()
+        {
+            foreach (var child in m_children)
+            {
+                child.Initialize();
+            }
+        }
+
+        public virtual void Draw(float flDeltaTime, SpriteBatch spriteBatch)
+        {
+            Debug.Assert(spriteBatch != null);
+
+            foreach (var child in m_children)
+            {
+                child.Draw(flDeltaTime, spriteBatch);
+            }
+        }
+
+        public virtual void Update(float flDeltaTime)
+        {
+            foreach (var child in m_children)
+            {
+                child.Update(flDeltaTime);
+            }
+        }
+
+        public void AddChild(SceneNode child)
+        {
+            Debug.Assert(child != null);
+            if (!m_children.Contains(child))
+            {
+                child.SetParent(this);
+                m_children.Add(child);
+            }
+        }
+
+        public void RemoveChild(SceneNode child)
+        {
+            Debug.Assert(child != null);
+            if (m_children.Contains(child))
+            {
+                child.SetParent(null);
+                m_children.Remove(child);
+            }
+        }
+
+        protected void SetParent(SceneNode parent)
+        {
+            if (m_parent != null)
+            {
+                Debug.Assert(m_parent.m_children.Contains(this));
+                m_parent.m_children.Remove(this);
+            }
+
+            m_vPositionLocal = parent == null ? Vector2.Zero : PositionWorld - parent.PositionWorld;
+            m_parent = parent;
+        }
+
+        protected virtual void OnTransformChanged()
+        {
+            foreach (var child in m_children)
+            {
+                child.m_vPositionWorld = PositionWorld + child.PositionLocal;
+                child.OnTransformChanged();
+            }
+        }
+    }
+}
